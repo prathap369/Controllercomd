189407dc4c2fecedcf0be21bb7bb3fe9ef2ceab0
prathap369 <137623929+prathap369@users.noreply.github.com>
2024-01-29 12:54:47 +0530

    hubs connect

---
+++ CtrlHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ctrlcmd.Hubs
+{
+    [HubName("CtrlHub")]
+    public class CtrlHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<CtrlHub>();
+            context.Clients.All.addNew(name, message);
+        }
+    }
+}
